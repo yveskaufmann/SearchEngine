1685d768e56f42f5bc8c178bdbc2e7fb847e62be
Yves Kaufmann <fxdapokalypse@googlemail.com>
2015-04-30 01:02:49 +0200

    Fügt einen UnitTest für den HTML Parser  hinzu

--- parser/test_html.py
+++ parser/test_html.py
@@ -1,21 +1,38 @@
+__author__ = 'Yves'
+
 import unittest
 from parser.html import HTMLParser
+from os.path import join, dirname 
 
 class TestHTMLParser(unittest.TestCase):
     
-    def test_parseDummyPage_ContentIsExtracted(self):
-        parser = HTMLParser()
-        dummy_page = self.get_dummy_page()
-        
-        parser.parse(dummy_page)
-
+    def test_parseDummyPage_LinksShouldBeExtracted(self):
+        parse_result = self.parse_page()
         expected_links = [ 'd{0:02}.html'.format(i)  for i in range(2, 5) ]
-        self.assertListEqual(parser.get_out_links(), expected_links) 
+        self.assertListEqual(parse_result.get_out_links(), expected_links) 
+
+    def test_parseDummyPage_LeadingAndTraillingWhitespacesShouldRemovedFromContent(self):
+        parse_result = self.parse_page()
+        self.assertRegex(parse_result.content, '^[^\s]+.*[^\s]$')
+    
+    def test_parseDummyPage_ContentIsExtracted(self):
+        expected_content = 'Python hates me :)'
+        parse_result = self.parse_page('<html><body>' + expected_content + '</body></html>')
+        self.assertEqual(expected_content, parse_result.content)
+    
+    def parse_page(self, page=''):
+        parser = HTMLParser()
+        parser.parse(self.get_dummy_page() if page == '' else page)
+        return parser
 
     def get_dummy_page(self):
-        with open('dummy.html') as dummy_file:
+        dummy_file_path = join(dirname(__file__), 'dummy.html')
+        with open(dummy_file_path) as dummy_file:
             return dummy_file.read()
+
+
 
 
 if __name__ == '__main__':
     unittest.main()
+
