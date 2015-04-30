2a46118b5f9b1e22444ac47c0f6100098ed5b1a6
pascalweiss <pascal@mbp111.local>
2015-04-30 11:44:55 +0200

    new functions for crawler. better names for document folders

--- crawler/Crawler.py
+++ crawler/Crawler.py
@@ -2,55 +2,48 @@ __author__ = 'pascal'
 
 from bs4 import BeautifulSoup
 from model.model import Page
+import re
+
 import os
-
-def get_path_for_material(file_name):
-    material_dir = os.path.join(os.getcwd(), os.pardir, "unterlagen/material")
-    return os.path.join(material_dir, file_name)
-
-def print_page(page):
-    print("\n\n" + page.title + " \n===")
-    print("url: " + page.url)
-    print("content: " + page.content)
-    print("out_links: ")
-    for link in page.out_links:
-        print(link)
 
 class Crawler:
 
     data = []
 
-    def string_for_url(self, url):
+    @staticmethod
+    def string_for_url(url):
         with open(url, "r") as file:
             return file.read()
 
-    def already_fetched_url(self, url):
-        for page in self.data:
-            if page.url == url:
-                return True
-        return False
+    @staticmethod
+    def get_path_for_material(file_name):
+        material_dir = os.path.join(os.getcwd(), os.pardir, "docs","ressources")
+        return os.path.join(material_dir, file_name)
 
-    def put_url_in_cache(self, url):
-        if url not in self.url_cache:
-                self.url_cache.append(url)
+    @staticmethod
+    def print_page(page):
+        """handy method for printing Page objects"""
+        print("\n\n" + page.title + " \n===")
+        print("url: \n¯¯¯¯\n" + page.url)
+        print("content: \n¯¯¯¯¯¯¯¯\n" + page.content)
+        print("out_links: \n¯¯¯¯¯¯¯¯¯¯")
+        for link in page.out_links:
+            print(link)
 
-    def extract_data_and_get_out_links(self, url):
-        txt = self.string_for_url(url)
-        soup = BeautifulSoup(txt)
-        page = Page()
-        page.url = url
-        page.title = soup.title.string
-        soup.title.clear()
-        out_links = []
-        for out_link in soup.findAll("a"):
-            out_links.append(get_path_for_material(out_link["href"]))
-            out_link.clear()
-        page.out_links = out_links
-        page.content = soup.get_text()
-        self.data.append(page)
-        return out_links
 
-    def merge_without_duplicates(self, target_list, source_list):
+    @staticmethod
+    def remove_empty_lines(txt):
+        new_txt = ""
+        for line in txt.splitlines():
+            regex = re.compile(r"^[^\s][^\n][a-zA-Z0-9]*.*$")
+            if regex.match(line):
+                new_txt += line + "\n"
+        return new_txt
+
+    @staticmethod
+    def merge_without_duplicates(target_list, source_list):
+        """Merges source_list into target_list without creating duplicates.
+        Returns all elements, that weren't in target_list before"""
         unseen_list = []
         for url in source_list:
             if url not in target_list:
@@ -58,12 +51,68 @@ class Crawler:
                 unseen_list.append(url)
         return unseen_list
 
-    def calculate_difference(self, target_list, source_list):
+    @staticmethod
+    def calculate_difference(target_list, source_list):
+        """Returns all elements from sour_list, that aren't in target_list."""
         difference = []
         for url in source_list:
             if url not in target_list:
                 difference.append(url)
         return difference
+
+    def already_fetched_url(self, url):
+        for page in self.data:
+            if page.url == url:
+                return True
+        return False
+
+    def get_link_structure_text(self):
+        """Prints an array of pages, just like in link_structure.txt"""
+        sorted_pages = sorted(self.data, key= lambda p: p.title)
+        result = ""
+        for page in sorted_pages:
+            outlink_titles = ""
+            count = 0
+            for url in page.out_links:
+                count += 1
+                link = self.page_for_url(url)
+                outlink_titles += link.title
+                if count != len(page.out_links):
+                    outlink_titles += ","
+            result += page.title + ":" + outlink_titles + "\n"
+        return result
+
+
+    def put_url_in_cache(self, url):
+        if url not in self.url_cache:
+                self.url_cache.append(url)
+
+    def page_for_url(self, url):
+        for page in self.data:
+            if page.url == url:
+                return page
+
+    def page_for_title(self, title):
+        for page in self.data:
+            if page.title == title:
+                return page
+
+    def extract_data_and_get_out_links(self, url):
+        txt = Crawler.string_for_url(url)
+        soup = BeautifulSoup(txt)
+        page = Page()
+        page.url = url
+        page.title = soup.title.string
+        soup.title.clear()
+        out_links = []
+        for out_link in soup.findAll("a"):
+            out_links.append(Crawler.get_path_for_material(out_link["href"]))
+            out_link.clear()
+        page.out_links = out_links
+        content = Crawler.remove_empty_lines(soup.get_text())
+        page.content = content
+        self.data.append(page)
+        return out_links
 
     def start_crawling(self, seed):
         self.url_cache = list(seed)
@@ -72,7 +121,6 @@ class Crawler:
             unseen_links = []
             while True:
                 url = self.url_cache.pop(0)
-                # OUTLINKS
                 out_links = (self.extract_data_and_get_out_links(url))
                 tmp_links = self.merge_without_duplicates(url_db, out_links)
                 self.merge_without_duplicates(unseen_links, tmp_links)
@@ -81,29 +129,35 @@ class Crawler:
                     break
 
 
-d01 = get_path_for_material("d01.html")
-d02 = get_path_for_material("d02.html")
-d03 = get_path_for_material("d03.html")
-d04 = get_path_for_material("d04.html")
-d05 = get_path_for_material("d05.html")
-d06 = get_path_for_material("d06.html")
-d07 = get_path_for_material("d07.html")
-d08 = get_path_for_material("d08.html")
 
+#  _____                           _
+# | ____|_  ____ _ _ __ ___  _ __ | | ___
+# |  _| \ \/ / _` | '_ ` _ \| '_ \| |/ _ \
+# | |___ >  < (_| | | | | | | |_) | |  __/
+# |_____/_/\_\__,_|_| |_| |_| .__/|_|\___|
+#                           |_|
 
+# Create a seed
+seed = [
+    Crawler.get_path_for_material("d01.html"),
+    Crawler.get_path_for_material("d06.html"),
+    Crawler.get_path_for_material("d08.html")
+]
 
-
-
-# instatiate the crawler
+# Instatiate the crawler.
 crawler = Crawler()
 
-# start the crawler...
-crawler.start_crawling([d01, d06, d08])
+# Start the crawler with the seed.
+crawler.start_crawling(seed)
 
-# get the data...
+# Access the data.
 crawler.data
 
-# print the data...
+# Print the data...
 for page in crawler.data:
-# ... with print_page(page_object)
-    print_page(page)
+# # ... with print_page(page_object)
+    Crawler.print_page(page)
+
+# Print the link structure
+link_structure_txt = crawler.get_link_structure_text()
+print(link_structure_txt)
