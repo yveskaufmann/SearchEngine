dd41847c19ddf506a8e08d9f49b1badf4ccad37e
Yves Kaufmann <fxdapokalypse@googlemail.com>
2015-05-04 18:09:08 +0200

    Ensures that the indexer index a page only once.

--- indexer/indexer.py
+++ indexer/indexer.py
@@ -4,9 +4,10 @@ from model.model import Index
 
 class Indexer:
 	""" Build an inverted index from assigned page objects. """
-	
+
 	def __init__(self):
 		self.__index = Index()
+		self.__already_indexed_pages = set()
 
 	def index_pages(self, *pages):
 		""" Index a sequence of pages """
@@ -15,9 +16,17 @@ class Indexer:
 
 	def index_page(self, page):
 		""" Index a single page """
+		
+		# Prevent that a page is indexed multiple times
+		if page.title in self.__already_indexed_pages: 
+			return
+
 		lexer = TokenLexer(page)
 		for token in lexer.tokens():
 			self.__index.register_Token(token, page.title)
+
+		# Memorize already indexed pages by his title
+		self.__already_indexed_pages.add(page.title)
 
 	@property
 	def index (self):
